85566f961fbca7f81a4fe939b9949b2cab7d7dc5
MKGus <shen3@DESKTOP-P990S44>
2017-03-10 10:18:02 +0800

    创建项目

---
+++ BookStore/NIIT.BookStore.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NIIT.BookStore.web.MvcApplication" Language="C#" %>
---
+++ BookStore/NIIT.BookStore.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NIIT.BookStore.web.Startup))]
+namespace NIIT.BookStore.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
